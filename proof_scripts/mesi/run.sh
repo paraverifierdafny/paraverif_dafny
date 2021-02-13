6cb0940a500f41c62731e7d8881c1d4d3e692f16
paraverifierdafny <51194501005@stu.ecnu.edu.cn>
2021-02-13 11:27:33 +0800

    modify

--- proof_scripts/mesi/run.sh
+++ proof_scripts/mesi/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 shopt -s expand_aliases
 source ~/.bashrc
-isabelle build -v -d . -b paraTheory_Session
-isabelle build -v -d . -b n_mesi_base_Session
-isabelle build -v -d . -b n_mesi_lemma_on_inv__1_Session
-isabelle build -v -d . -b n_mesi_lemma_on_inv__2_Session
-isabelle build -v -d . -b n_mesi_lemma_on_inv__3_Session
-isabelle build -v -d . -b n_mesi_lemma_invs_on_rules_Session
-isabelle build -v -d . -b n_mesi_invs_on_inis_Session
-isabelle build -v -d . -b n_mesi_Session
+Start=`date +%F | sed 's/-//g'``date +%T | sed 's/://g'`
+dafny n_mesi_lemma_oninv__1.dfy
+dafny n_mesi_lemma_oninv__2.dfy
+dafny n_mesi_lemma_oninv__3.dfy
+End=`date +%F | sed 's/-//g'``date +%T | sed 's/://g'`
+val=`expr $End - $Start`
+printf "%s %.2f sec\n" 运行时间为： $val
+
